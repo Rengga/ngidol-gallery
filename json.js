0d66645a9bfa71a0cc9c458d8763b334b30960c5
Rengga <renggaarnanta@gmail.com>
2024-04-15 22:14:18 +0700

    fixing

---
+++ json.js
@@ -0,0 +1,86 @@
+var json = [
+  {
+    full: "https://i.ibb.co/8mcs8JX/ssssskkkv2.jpg",
+    thumb: "https://i.ibb.co/Cw280qW/ssssskkkv2.jpg",
+  },
+  {
+    full: "https://i.ibb.co/7CrpKW0/ddff2.jpg",
+    thumb: "https://i.ibb.co/GpCMR2r/ddff2.jpg",
+  },
+  {
+    full: "https://i.ibb.co/yQVSnTJ/hjb-v1psd.jpg",
+    thumb: "https://i.ibb.co/mcS8Cs7/hjb-v1psd.jpg",
+  },
+  {
+    full: "https://i.ibb.co/Fw1wShp/kqng-2.jpg",
+    thumb: "https://i.ibb.co/Sngnh6S/kqng-2.jpg",
+  },
+  {
+    full: "https://i.ibb.co/5Kmphc6/kqng.jpg",
+    thumb: "https://i.ibb.co/Z254zTV/kqng.jpg",
+  },
+  {
+    full: "https://i.ibb.co/zsfMhBV/htmpth.png",
+    thumb: "https://i.ibb.co/rkyjGz2/htmpth.png",
+  },
+  {
+    full: "https://i.ibb.co/TYV6Myf/1eeer.jpg",
+    thumb: "https://i.ibb.co/ZGn5z9j/1eeer.jpg",
+  },
+  {
+    full: "https://i.ibb.co/FJgTnhc/drr.jpg",
+    thumb: "https://i.ibb.co/jGgXH8q/drr.jpg",
+  },
+  {
+    full: "https://i.ibb.co/5vj40Z9/fna.png",
+    thumb: "https://i.ibb.co/pZKw7qP/fna.png",
+  },
+  {
+    full: "https://i.ibb.co/qF38RWZ/sds.jpg",
+    thumb: "https://i.ibb.co/rfRhZcW/sds.jpg",
+  },
+  {
+    full: "https://i.ibb.co/hMjtYRn/hem-purple.jpg",
+    thumb: "https://i.ibb.co/g7pB4MX/hem-purple.jpg",
+  },
+  {
+    full: "https://i.ibb.co/5jYggmF/Bbrr.jpg",
+    thumb: "https://i.ibb.co/c8bSS0L/Bbrr.jpg",
+  },
+  {
+    full: "https://i.ibb.co/hCHBy0m/star.png",
+    thumb: "https://i.ibb.co/HXYtVZH/star.png",
+  },
+  {
+    full: "https://i.ibb.co/ww1NQ2R/cklter.png",
+    thumb: "https://i.ibb.co/tx5M214/cklter.png",
+  },
+  {
+    full: "https://i.ibb.co/N7gvQbN/ble.png",
+    thumb: "https://i.ibb.co/JCW10gp/ble.png",
+  },
+  {
+    full: "https://i.ibb.co/6NyHJjY/lia.png",
+    thumb: "https://i.ibb.co/SJ6t7FN/lia.png",
+  },
+  {
+    full: "https://i.ibb.co/zfpsdwL/gtmm.jpg",
+    thumb: "https://i.ibb.co/7yPvhFT/gtmm.jpg",
+  },
+  {
+    full: "https://i.ibb.co/pxrD4SC/azz.png",
+    thumb: "https://i.ibb.co/s2JSsZL/azz.png",
+  },
+  {
+    full: "https://i.ibb.co/L016t13/kaela.jpg",
+    thumb: "https://i.ibb.co/tHDcsD9/kaela.jpg",
+  },
+  {
+    full: "https://i.ibb.co/9yqDRLn/ngdlmaki.jpg",
+    thumb: "https://i.ibb.co/TPHfzSc/ngdlmaki.jpg",
+  },
+  {
+    full: "https://i.ibb.co/MMQL7wc/kbo.jpg",
+    thumb: "https://i.ibb.co/pJ5pdVf/kbo.jpg",
+  },
+];
